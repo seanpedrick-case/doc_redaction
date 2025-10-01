64e67bd3214c87a0d8f916c277b249c732d1197d
Sean Pedrick-Case <spedrickcase@lambeth.gov.uk>
2025-10-01 10:23:58 +0100

    Updated entrypoint to match root path defined in main app

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,15 +17,15 @@ else
         
         GRADIO_SERVER_NAME=${GRADIO_SERVER_NAME:-0.0.0.0}
         GRADIO_SERVER_PORT=${GRADIO_SERVER_PORT:-7860}
-        GRADIO_ROOT_PATH=${GRADIO_ROOT_PATH:-/}
+        ROOT_PATH=${ROOT_PATH:-/}
 
         # Start uvicorn server.
-        echo "Starting with Uvicorn on $GRADIO_SERVER_NAME:$GRADIO_SERVER_PORT with root path $GRADIO_ROOT_PATH"
+        echo "Starting with Uvicorn on $GRADIO_SERVER_NAME:$GRADIO_SERVER_PORT with root path $ROOT_PATH"
         exec uvicorn app:app \
             --host $GRADIO_SERVER_NAME \
             --port $GRADIO_SERVER_PORT \
             --proxy-headers \
-            --root-path $GRADIO_ROOT_PATH
+            --root-path $ROOT_PATH
     else
         echo "Starting in Gradio mode..."
         exec python app.py
